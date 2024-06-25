a8bfcde31cdf4dab3ee2077e8ff5849720c53c62
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2024-06-25 14:39:45 +0700

    first commit

---
+++ app/Controllers/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class Test extends BaseController
+{
+    public function getPesan($nama = "Pengunjung")
+    {
+        $data = [
+            "pesan" => "Selamat datang",
+            "nama" => $nama,
+        ];
+        return view("test", $data);
+    }
+
+    public function getLokasi()
+    {
+        return "Lokasi anda di Focus Techno Media";
+    }
+}
