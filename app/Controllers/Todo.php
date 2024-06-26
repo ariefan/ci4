cd40c2e93f167ea6ffe000d11430dafa47cfea45
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2024-06-26 11:19:09 +0700

    update controller

--- app/Controllers/Todo.php
+++ app/Controllers/Todo.php
@@ -7,8 +7,54 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class Todo extends BaseController
 {
-    public function index()
+    public function getIndex()
     {
-        //
+        $todoModel = new \App\Models\Todo();
+        $todos = $todoModel->findAll();
+        $data = [
+            'rows' => $todos,
+        ];
+        return view('todo/index', $data);
+    }
+    public function getNew()
+    {
+        $todoModel = new \App\Models\Todo();
+        $data = [
+            'row' => $todoModel,
+            'action' => 'create',
+        ];
+        return view('todo/form', $data);
+    }
+    public function postCreate()
+    {
+        $data = $this->request->getPost();
+        $todoModel = new \App\Models\Todo();
+        $todoModel->insert($data);
+        session()->setFlashdata('success', 'Data berhasil disimpan');
+        $this->response->redirect(site_url('todo'));
+    }
+    public function getEdit($id)
+    {
+        $todoModel = new \App\Models\Todo();
+        $data = [
+            'row' => $todoModel->find($id),
+            'action' => 'update',
+        ];
+        return view('todo/form', $data);
+    }
+    public function postUpdate($id)
+    {
+        $data = $this->request->getPost();
+        $todoModel = new \App\Models\Todo();
+        $todoModel->update($id, $data);
+        session()->setFlashdata('success', 'Data berhasil disimpan');
+        $this->response->redirect(site_url('todo'));
+    }
+    public function postDelete($id)
+    {
+        $todoModel = new \App\Models\Todo();
+        $todoModel->delete($id);
+        session()->setFlashdata('success', 'Data berhasil dihapus');
+        $this->response->redirect(site_url('todo'));
     }
 }
