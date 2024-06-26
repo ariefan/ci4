c0d735bff9f0207931a5dbd5843ffcdd3489db1c
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2024-06-26 11:38:07 +0700

    form

---
+++ app/Views/todo/form.php
@@ -0,0 +1,18 @@
+<?= $this->extend('layout/app') ?>
+<?= $this->section('content') ?>
+<h2>Input Todo</h2>
+<form method="post" action="<?= site_url('todo/' . $action . ($action ==
+                                'update' ? '/' . $row->id : '')); ?>">
+    <div class="mb-3">
+        <label class="form-label">Judul</label>
+        <input type="text" class="form-control" name="judul" value="<?=
+                                                                    $row->judul; ?>">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Deskripsi</label>
+        <input type="text" class="form-control" name="deskripsi" value="<?=
+                                                                        $row->deskripsi; ?>">
+    </div>
+    <button type="submit" class="btn btn-primary">Simpan</button>
+</form>
+<?= $this->endSection() ?>
